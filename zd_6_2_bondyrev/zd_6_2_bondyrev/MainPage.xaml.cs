5d4a7a6552ac78a24a3dc8667fdb81f62edf89a8
dibalass <123352922+dibalass@users.noreply.github.com>
2023-06-22 12:56:23 +0500

    Add files via upload

---
+++ zd_6_2_bondyrev/zd_6_2_bondyrev/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace zd_6_2_bondyrev
+{
+    public partial class MainPage :ContentPage
+    {
+        public MainPage ()
+        {
+            InitializeComponent();
+        }
+    }
+}
